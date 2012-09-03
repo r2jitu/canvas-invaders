6c4a33279a606a64e09debdcc1dfea9ed899d413
Jitu Das <jitu@cmu.edu>
2012-09-02 20:45:06 -0400

    Added spaceship classes and added screen reset functions

--- index.js
+++ index.js
@@ -61,12 +61,12 @@ Game = (function () {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
 
-        this.curScreen = this.menu;
-
         // Compute update rate
         this.fps = Util.default_arg(config.fps, 60);
         this.delay = 1000 / this.fps;
         this.interval = null;
+
+        this.setScreen(this.menu);
 
         // Add event listeners
         var self = this;
@@ -108,6 +108,10 @@ Game = (function () {
         this.curScreen.render(this.ctx);
     };
 
+    Game.prototype.setScreen = function (screen) {
+        this.curScreen = screen;
+    }
+
     return Game;
 })();
 
@@ -119,6 +123,8 @@ Screen = (function () {
         ctx.fillStyle = game.bgColor;
         ctx.fillRect(0, 0, 600, 600);
     };
+
+    Screen.prototype.reset = function () { /* Do nothing */ };
 
     return Screen;
 })();
@@ -212,6 +218,29 @@ Object = (function () {
     };
 
     return Object;
+})();
+
+SpaceShip = (function () {
+    function SpaceShip() {
+    }
+
+    Util.extend(SpaceShip, Object);
+
+    return SpaceShip;
+})();
+
+Platoon = (function () {
+    function Platoon(startx, starty, config) {
+        this._super.prototype.constructor({
+        });
+    }
+
+    Util.extend(Platoon, Object);
+
+    Platoon.prototype.reset = function () {
+    };
+
+    return Platoon;
 })();
 
 Sprite = (function () {
